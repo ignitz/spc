d258925ffae20b4bb434dd23c2837634faab2f17
ignitz <ignitzhjfk@gmail.com>
2016-07-19 19:45:15 -0300

    1035 - TDA Racional (Ruby)

---
+++ ruby/1 - Iniciante/1035.rb
@@ -0,0 +1,11 @@
+entrada = gets.chomp.split(" ")
+a = entrada[0].to_i
+b = entrada[1].to_i
+c = entrada[2].to_i
+d = entrada[3].to_i
+
+if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0)
+	puts "Valores aceitos"
+else
+	puts "Valores nao aceitos"
+end
