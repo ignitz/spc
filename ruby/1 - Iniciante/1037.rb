3b7aad4f6c9b86e0db6a28b2cdc2f4ec7fe11480
ignitz <ignitzhjfk@gmail.com>
2016-07-19 21:34:44 -0300

    1037 - Intervalo (Ruby)
    Com problema desconhecido ainda.
    Tentei BigDecimal mas aparenta ter algum problema.
    Utilizei um código em C com float como e teste e passou.

---
+++ ruby/1 - Iniciante/1037.rb
@@ -0,0 +1,23 @@
+require 'bigdecimal'
+
+# puts  123.6 - 123
+# x = BigDecimal("123.6") - BigDecimal("123")
+# puts x.to_f
+
+entrada = BigDecimal(gets.chomp.to_s)
+if entrada >= 0
+	if entrada <= 25.0
+		puts "Intervalo [0,25]"
+	elsif entrada <= 50.0
+		puts "Intervalo (25,50]"
+	elsif entrada <= 75.0
+		puts "Intervalo (50,75]"
+	elsif entrada <= 100.0
+		puts "Intervalo (75,100]"
+	else
+		puts "Fora do intervalo"
+	end
+else
+	puts "Fora do intervalo"
+end
+
