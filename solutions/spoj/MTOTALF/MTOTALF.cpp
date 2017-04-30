ef1737f4a1ca8ac8d363d1e799e7875e02731009
ignitz <ignitzhjfk@gmail.com>
2017-04-29 21:55:08 -0300

    step

--- solutions/spoj/MTOTALF/MTOTALF.cpp
+++ solutions/spoj/MTOTALF/MTOTALF.cpp
@@ -45,20 +45,18 @@ Graph::Graph () {
 Graph::~Graph () {}
 
 void Graph::set_weight(char u, char v, int weight) {
-  // std::cerr << "Setting edge weight" << '\n';
-  // std::cerr << u << ' ' << v << " " << weight << '\n';
-  // int i = (u != 'Z') ? int(u) - int('A') : this->n;
-  // int j = (v != 'Z') ? int(v) - int('A') : this->n;
+  //std::cerr << "Setting edge weight" << '\n';
+  //std::cerr << u << ' ' << v << " " << weight << '\n';
   int i = (int(u) < int('a') ) ? int(u) - int('A') : int(u) - int('a') + int('Z') - int('A');
   int j = (int(v) < int('a') ) ? int(v) - int('A') : int(v) - int('a') + int('Z') - int('A');
-  // std::cerr << i << ' ' << j << '\n';
+  //std::cerr << i << ' ' << j << '\n';
   // if already have, just plus the weight
   this->edges[ i ][ j ] += weight;
   this->edges[ j ][ i ] += weight;
 }
 
 bool Graph::bfs( int init_v ) {
-  std::cerr << "exec bfs" << '\n';
+  std::cerr << "\033[1;37m" << "exec bfs" << '\n' << "\033[0m";
   std::vector<bool> colors;
   colors.resize(this->n, false);
 
@@ -70,10 +68,10 @@ bool Graph::bfs( int init_v ) {
 
   while (!queue.empty()) {
     u = queue.front();
-    std::cerr << u << '\t';
+    std::cerr << "\033[1;31m" << u << '\t' << "\033[0m";
     queue.pop();
     if (u == SINK) {
-      std::cerr << '\n';
+      std::cerr << "\033[1;31m" << '\n' << "\033[0m";
       return true;
     }
     for (int v = 0; v < this->n; v++) {
@@ -84,12 +82,12 @@ bool Graph::bfs( int init_v ) {
       }
     }
   }
-  std::cerr << '\n';
+  std::cerr << "\033[1;31m" << '\n' << "\033[0m";
   return false;
 }
 
 bool Graph::dfs( int init_v ) {
-  std::cerr << "exec bfs" << '\n';
+  std::cerr << "\033[1;37m" << "exec dfs" << '\n' << "\033[0m";
   std::vector<bool> colors;
   colors.resize(this->n, false);
 
@@ -101,10 +99,10 @@ bool Graph::dfs( int init_v ) {
 
   while (!mystack.empty()) {
     u = mystack.top();
-    std::cerr << u << '\t';
+    std::cerr << "\033[1;31m" << u << '\t' << "\033[0m";
     mystack.pop();
     if (u == SINK) {
-      std::cerr << '\n';
+      std::cerr << "\033[1;31m" << '\n' << "\033[0m";
       return true;
     }
     for (int v = 0; v < this->n; v++) {
@@ -115,17 +113,22 @@ bool Graph::dfs( int init_v ) {
       }
     }
   }
-  std::cerr << '\n';
+  std::cerr << "\033[1;31m" << '\n' << "\033[0m";
   return false;
 }
 
 int Graph::FordFulkerson() {
-  std::cerr << "initint totalflow" << '\n';
+  std::cerr << "\033[1;37m" << "exec FordFulkerson" << '\n' << "\033[0m";
   int response = 0;
   int path_flow;
   std::fill(this->parent.begin(), this->parent.end(), -1);
 
   while (this->dfs(0)) {
+
+    for (int i = 0; i < this->n; i++) {
+      std::cerr << "\033[1;36m" << this->parent[i] << ' ' << "\033[0m";
+    }
+    std::cerr << "\033[1;31m" << '\n' << "\033[0m";
 
     // Find minimum residual capacity of the edges along the
     // path filled by BFS. Or we can say find the maximum flow
@@ -138,7 +141,7 @@ int Graph::FordFulkerson() {
       s = parent[s];
     }
 
-    std::cerr << "get path_flow " << path_flow << '\n';
+    std::cerr << "\033[1;32m" << "get path_flow " << path_flow << '\n' << "\033[0m";
     response += path_flow; // Add path flow to overall flow
 
     // update residual capacities of the edges and reverse edges
@@ -151,8 +154,9 @@ int Graph::FordFulkerson() {
       this->edges[v][u] += path_flow;
       v = this->parent[v];
     }
+    std::cerr << "\033[1;33m" << "max_flow " << response << '\n' << "\033[0m";
   }
-  std::cerr << "response " << response << '\n';
+  std::cerr << "\033[1;33m" << "response " << response << '\n' << "\033[0m";
   return response;
 }
 
