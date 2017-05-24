dfa1d6301a10e01cb6ddf371580fbe52b09a209c
yuri diego santos niitsuma <yuriniitsuma@paracatu.grad.dcc.ufmg.br>
2017-05-24 13:56:17 -0300

    work in progress in 1028, need euclides algorithm to be faster

---
+++ solutions/uri/5 - Matemática/1028/1028.cpp
@@ -0,0 +1,42 @@
+/*
+ * Yuri Niitsuma
+ * 2011039023
+ * https://www.urionlinejudge.com.br/judge/pt/problems/view/1028
+ * 1028 - Figurinhas
+*/
+
+#include <iostream>
+
+int mdc (int n, int m) {
+    int max_div = std::max(n/2, m/2);
+
+    int max_div_common = 1;
+    // first find all even numbers
+    while ( (n % 2 == 0) && (m % 2 == 0) ) {
+        max_div_common *= 2;
+        n /= 2; m /= 2;
+    }
+
+    // find all odd numbers
+    int i = 3;
+    while ( i <= max_div ) {
+        if ( (n % i == 0) && (m % i == 0) ) {
+            max_div_common *= i;
+            n /= i; m /= i;
+        }
+        else {
+            i += 2;
+        }
+    }
+    return max_div_common;
+}
+
+int main() {
+    int number_of_tests, f1, f2;
+    std::cin >> number_of_tests;
+    for (int i = 0; i < number_of_tests; i++) {
+        std::cin >> f1 >> f2;
+        std::cout << mdc(f1, f2) << std::endl;
+    }
+	return 0;
+}
