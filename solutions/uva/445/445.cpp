994a3563bb50fc24726c7b879ec7cf07bb0f2574
ignitz <ignitzhjfk@gmail.com>
2019-03-17 01:43:29 -0300

    UVA 445 - Marvelous Mazes

--- solutions/uva/445/445.cpp
+++ solutions/uva/445/445.cpp
@@ -7,8 +7,53 @@
 
 #include <iostream>
 
+// read numbers
+// read letter
+// read !
+// let's go
+
+std::string drawMaze(std::string line)
+{
+	char toAdd;
+	std::string response;
+	int manyCharTimes = 0;
+
+	for (auto &c : line)
+	{
+		if (c == '!')
+		{
+			response += '\n';
+			continue;
+		}
+		if (isdigit(c))
+		{
+			manyCharTimes += (int)(c - '0');
+			continue;
+		}
+		else
+		{
+			toAdd = (c == 'b') ? ' ' : c;
+			for (int i = 0; i < manyCharTimes; i++)
+			{
+				response += toAdd;
+			}
+			manyCharTimes = 0;
+			continue;
+		}
+	}
+
+	return response;
+}
+
 int main()
 {
+	std::string line, output;
+
+	while (std::getline(std::cin, line))
+	{
+		output = drawMaze(line);
+		std::cout << output << '\n';
+	}
 	// code
 	return 0;
 }
